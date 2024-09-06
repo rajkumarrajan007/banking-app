802817fd51feddd2657e7ef8ae34c32093ddfdd0
raj <rajkumarrajan009@gmail.com>
2024-09-06 17:35:12 +0530

    Just checking git commit

--- src/main/java/com/raj/banking/BankingAppApplication.java
+++ src/main/java/com/raj/banking/BankingAppApplication.java
@@ -1,6 +1,7 @@
 package com.raj.banking;
 
 import org.springframework.boot.SpringApplication;
+// I am writing this this to check git
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
